5cd96fad191e16f3152182ef00258ddf7216b003
Tsai Chen Ping <oscar60310@gmail.com>
2016-12-31 19:08:09 +0800

    init

---
+++ main.js
@@ -0,0 +1,53 @@
+var target;
+function start() {
+    if (getUrlParameter('title') == null || getUrlParameter('time') == null) {
+        $('#title').html("資訊不完整");
+        return;
+    }
+    $('#title').html(getUrlParameter('title'));
+    var times = getUrlParameter('time').split('/');
+    target = new Date();
+    target.setFullYear(parseInt(times[0]));
+    target.setMonth(parseInt(times[1]) - 1);
+    target.setDate(parseInt(times[2]));
+    target.setHours(parseInt(times[3]));
+    target.setMinutes(parseInt(times[4]));
+    target.setSeconds(parseInt(times[5]));
+
+
+    setTimeout(count, 1000);
+
+}
+function count() {
+    var now = new Date();
+    var bet = Math.floor((target.getTime() - now.getTime()) / 1000);
+    if (bet < 0) {
+        return;
+    }
+
+    console.log(bet);
+    $('#day').html(Math.floor(bet / 60 / 60 / 24));
+    bet %= 60 * 60 * 24;
+    $('#hour').html(Math.floor(bet / 60 / 60));
+    bet %= 60 * 60;
+    $('#min').html(Math.floor(bet / 60));
+    bet %= 60;
+    $('#sec').html(Math.floor(bet));
+
+    setTimeout(count, 1000);
+}
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
+start();
